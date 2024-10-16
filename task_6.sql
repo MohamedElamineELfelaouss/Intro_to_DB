d1bfc118e0d7df961f29f62ad7d9e889a2caad91
dazelAMINE <amineelmohamed4@gmail.com>
2024-10-16 20:19:11 +0100

    edited task 6 file

--- task_6.sql
+++ task_6.sql
@@ -1,4 +1,5 @@
-INSERT INTO customer (customer_id, customer_name, email, address)
+-- Insert multiple rows into the Customers table with correct address formatting
+INSERT INTO Customers (customer_id, customer_name, email, address)
 VALUES 
 (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
 (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
