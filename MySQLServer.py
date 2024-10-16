6d1a8ddfa239f63dcada788b51076da4689db9d8
dazelAMINE <amineelmohamed4@gmail.com>
2024-10-16 19:51:11 +0100

    added db connection file

---
+++ MySQLServer.py
@@ -0,0 +1,37 @@
+import mysql.connector
+from mysql.connector import Error
+
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+def create_database():
+    try:
+        mysql_root_password = os.getenv('MY_SQL_ROOT_PASSWD')
+        connection = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password=mysql_root_password
+        )
+        
+        if connection.is_connected():
+            cursor = connection.cursor()
+            
+            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+            print("Database 'alx_book_store' created successfully")
+            
+    except Error as e:
+        print(f"The error '{e}' occurred")
+    
+    finally:
+        
+        if (connection.is_connected()):
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed.")
+
+if __name__ == "__main__":
+    create_database()
+    
