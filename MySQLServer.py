52404a2c7089533949d63bb8bc086d6823d545a8
dazelAMINE <amineelmohamed4@gmail.com>
2024-10-16 19:52:26 +0100

    edited mysql server file

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,5 +1,5 @@
 import mysql.connector
-from mysql.connector import Error
+from mysql.connector
 
 from dotenv import load_dotenv
 import os
@@ -22,7 +22,7 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully")
             
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"The error '{e}' occurred")
     
     finally:
